7de1259de99f7c66b8bb9d50ff087db994bb8623
Gertvaaborta <lololo822228822228822228@gmail.com>
2025-11-21 23:01:18 +0300

    Create figure.h

---
+++ include/figure.h
@@ -0,0 +1,23 @@
+#ifndef FIGURE_H
+#define FIGURE_H
+
+#include "point.h"
+#include <memory>
+#include <iostream>
+
+template<Scalar T>
+class Figure {
+public:
+    virtual ~Figure() = default;
+    
+    virtual double area() const = 0;
+    virtual Point<T> geometricCenter() const = 0;
+    virtual void printVertices() const = 0;
+    virtual operator double() const { return area(); }
+    
+    virtual bool operator==(const Figure& other) const {
+        return area() == other.area();
+    }
+};
+
+#endif
