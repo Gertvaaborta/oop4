6d6bc746a2d8771c26b9e2a2b35512132dd3142f
Gertvaaborta <lololo822228822228822228@gmail.com>
2025-11-21 23:04:36 +0300

    Update triangle.h

--- include/triangle.h
+++ include/triangle.h
@@ -8,15 +8,40 @@ class Triangle : public Figure<T> {
 private:
     std::unique_ptr<Point<T>> p1, p2, p3;
 public:
-    Triangle(Point<T> a, Point<T> b, Point<T> c);
-    double area() const override;
-    Point<T> geometricCenter() const override;
-    void printVertices() const override;
+    Triangle(Point<T> a, Point<T> b, Point<T> c) 
+        : p1(std::make_unique<Point<T>>(a))
+        , p2(std::make_unique<Point<T>>(b))
+        , p3(std::make_unique<Point<T>>(c)) {}
     
-    Triangle(const Triangle& other);
-    Triangle& operator=(const Triangle& other);
+    double area() const override {
+        T x1 = p1->getX(), y1 = p1->getY();
+        T x2 = p2->getX(), y2 = p2->getY();
+        T x3 = p3->getX(), y3 = p3->getY();
+        
+        return std::abs((x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2)) / 2.0);
+    }
+    
+    Point<T> geometricCenter() const override {
+        return (*p1 + *p2 + *p3) / 3;
+    }
+    
+    void printVertices() const override {
+        std::cout << "Triangle vertices: " << *p1 << ", " << *p2 << ", " << *p3 << std::endl;
+    }
+    
+    Triangle(const Triangle& other) 
+        : p1(std::make_unique<Point<T>>(*other.p1))
+        , p2(std::make_unique<Point<T>>(*other.p2))
+        , p3(std::make_unique<Point<T>>(*other.p3)) {}
+    
+    Triangle& operator=(const Triangle& other) {
+        if (this != &other) {
+            p1 = std::make_unique<Point<T>>(*other.p1);
+            p2 = std::make_unique<Point<T>>(*other.p2);
+            p3 = std::make_unique<Point<T>>(*other.p3);
+        }
+        return *this;
+    }
 };
-
-#include "triangle.cpp"
 
 #endif
