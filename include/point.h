cde5da21aa1e853cc1ac7de5e47a799e72d3074c
Gertvaaborta <lololo822228822228822228@gmail.com>
2025-11-22 01:13:58 +0300

    Update point.h

--- include/point.h
+++ include/point.h
@@ -14,7 +14,47 @@ private:
     T x, y;
 public:
     Point(T x = 0, T y = 0) : x(x), y(y) {}
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     T getX() const { return x; }
     T getY() const { return y; }
     
@@ -32,7 +72,29 @@ public:
     Point operator/(T divisor) const {
         return Point(x / divisor, y / divisor);
     }
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     friend std::ostream& operator<<(std::ostream& os, const Point& point) {
         os << "(" << point.x << ", " << point.y << ")";
         return os;
